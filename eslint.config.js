131a626202362a4444f7bfa58a3e644af6062942
RajendraDamar <rajendra.damar.setiyawan@mail.ugm.ac.id>
2025-10-08 19:43:59 +0700

    init

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+/* eslint-env node */
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+    },
+  },
+  {
+    ignores: ['dist/*'],
+  },
+  {
+    rules: {
+      'react/display-name': 'off',
+    },
+  },
+]);
